fe989964402172a6e4612f0c98a9f079682bf971
Nel Barthelemy <nel_barthelemy@yahoo.com>
2020-08-27 21:54:19 -0400

    cerated yearArr and working on expenses output

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 //vars
 var initType;
 var expense= {type:"", amount:0.0, desc:"", month:"", week:"", day:""};
-var month =new Array(12);
-var week = new Array();
-var day =[0,0,0,0,0,0,0,0,0,0,0,0];
- for(let i =0;i<4;i++){
-    week[i] = day;
- }
- console.log("week");
- console.log(week);
- for(let i =0;i<12;i++){
-    month[i] = week;
- }
- console.log('month');
- console.log(month);
+var yearArr =[ 
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+                [[],[],[],[]],
+            ];
+ console.log(yearArr);
 //frontend
 function seeDiv(id){
     document.getElementById(id).className = "see inputs"; /*2 classes*/
@@ -30,17 +32,7 @@ for(let i =0; i <expButtons.length; i++)
 {
     expButtons[i].addEventListener("click",secondDiv);
 }
-
 //backend
-/*
-expButtons[0].addEventListener('click', setInitType1);
-expButtons[1].addEventListener('click', setInitType(expButtons[1].innerHTML));
-expButtons[2].addEventListener('click', setInitType(expButtons[2].innerHTML));
-expButtons[3].addEventListener('click', setInitType(expButtons[3].innerHTML));
-expButtons[4].addEventListener('click', setInitType(expButtons[4].innerHTML));
-expButtons[5].addEventListener('click', setInitType(expButtons[5].innerHTML));
-expButtons[6].addEventListener("click", setInitType(expButtons[6].innerHTML));*/
-
 function setInitType(val){
     initType = val;
     console.log(initType);
@@ -48,20 +40,35 @@ function setInitType(val){
 //when user clicks submit amount and description will go into the object
 function setObject(){//called by submit button
     expense.type =initType;
-    expense.amount = JSON.parse(document.getElementById("amount-input").value);
+    expense.amount = JSON.parse(document.getElementById("amount-input").value);//getting in5tput from the html document
     expense.desc = document.getElementById("desc-input").value;
+
     let d = new Date();
-    let month = JSON.stringify(d.getMonth()+1);
+    let monthHolder = JSON.stringify(d.getMonth()+1); //object.setMonth()
     let date = JSON.stringify(d.getDate());
     let year = JSON.stringify(d.getFullYear());
-    expense.month = JSON.parse(month)-1;
-    // expense.week =
-    // intDate = JSON.parse(date);
-    // if(intdate >0 && intdate<7)
-    //     expense.week =0;
-    // else if(intdate >=8 && intdate<=15)
-    expense.day = month+"/"+date+"/"+year;
-    noSeeDiv('input-div');
-    console.log(expense);
+    expense.month = JSON.parse(monthHolder)-1;
 
+    // expense.week =
+    intDate = JSON.parse(date);
+    if(intDate >0 && intDate<7)
+        expense.week =0;
+    else if(intDate >=8 && intDate<=15)//object.setWeek()
+        expense.week=1;
+    else if(intDate >=16 && intDate <=28)
+        expense.week=2;
+    else
+        expense.week=3;
+
+    expense.day = monthHolder+"/"+date+"/"+year;
+    toArray();
+    //console.log(expense)//when expense is stored in yearArr, its stored by reference so all elements would  be the same
+    noSeeDiv('input-div');
+   
+}
+function toArray(){
+    let expenseCopy =Object.assign({},expense);//shallow copy of the object since the object is stored by reference
+    console.log(expenseCopy);
+    yearArr[expense.month][expense.week].push(expenseCopy);
+    // yearArr[expense.month][expense.week].push(Object.assign({},expense)); //works too
 }
