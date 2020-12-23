bd5b1e97854d4ef1179bca7f0f8c6cc65bbcf6ed
Nel Barthelemy <nel_barthelemy@yahoo.com>
2020-12-23 13:23:45 -0500

    nothing new

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
 //vars
 var initType;
 var expense= {type:"", amount:0.0, desc:"", month:"", week:"", day:""};
-var yearArr =[ 
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
-                [[],[],[],[]],
+// localStorage.getItem("")
+var yearArr = localStorage.getItem("yearArray")?JSON.parse(localStorage.getItem("yearArray")): setArray();
+function setArray(){
+    let arr = [[],[],[],[],[],[],[],[],[],[],[],[]];
+    for(let i=0; i<12;i++)
+        arr[i] = [
+                {food:[],bills:[],tuition:[],clothing:[],gas_trans:[],entertainment:[],other:[],total:0},
+                {food:[],bills:[],tuition:[],clothing:[],gas_trans:[],entertainment:[],other:[],total:0},
+                {food:[],bills:[],tuition:[],clothing:[],gas_trans:[],entertainment:[],other:[],total:0},
+                {food:[],bills:[],tuition:[],clothing:[],gas_trans:[],entertainment:[],other:[],total:0}
             ];
+    localStorage.setItem("yearArray",JSON.stringify(arr));
+    return arr;
+}
+
  console.log(yearArr);
 //frontend
+
 function seeDiv(id){
     document.getElementById(id).className = "see inputs"; /*2 classes*/
 }
@@ -48,27 +50,36 @@ function setObject(){//called by submit button
     let date = JSON.stringify(d.getDate());
     let year = JSON.stringify(d.getFullYear());
     expense.month = JSON.parse(monthHolder)-1;
-
-    // expense.week =
-    intDate = JSON.parse(date);
-    if(intDate >0 && intDate<7)
-        expense.week =0;
-    else if(intDate >=8 && intDate<=15)//object.setWeek()
-        expense.week=1;
-    else if(intDate >=16 && intDate <=28)
-        expense.week=2;
-    else
-        expense.week=3;
-
+    expense.week =setWeek(date);
     expense.day = monthHolder+"/"+date+"/"+year;
     toArray();
     //console.log(expense)//when expense is stored in yearArr, its stored by reference so all elements would  be the same
     noSeeDiv('input-div');
    
 }
+function setWeek(date){
+    var intDate = JSON.parse(date);
+    var expWeek;
+    if(intDate >0 && intDate<=7)
+        expWeek =0;
+    else if(intDate >=8 && intDate<=15)//object.setWeek()
+        expWeek=1;
+    else if(intDate >=16 && intDate <=28)
+        expWeek=2;
+    else
+        expWeek=3;
+
+    return expWeek;
+}
+
 function toArray(){
     let expenseCopy =Object.assign({},expense);//shallow copy of the object since the object is stored by reference
     console.log(expenseCopy);
-    yearArr[expense.month][expense.week].push(expenseCopy);
+    var expType =(expense.type).toLowerCase();
+    yearArr[expense.month][expense.week][expType].push(expenseCopy);
+    yearArr[expense.month][expense.week].total +=  expenseCopy.amount; //total in yearArr = total + expensecopy.amount;
     // yearArr[expense.month][expense.week].push(Object.assign({},expense)); //works too
+    localStorage.setItem("yearArray",JSON.stringify(yearArr));
 }
+
+
